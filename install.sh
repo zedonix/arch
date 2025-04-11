3aef789e813cacf01b5819da1435656eb2784482
Zed <197267174+zedonix@users.noreply.github.com>
2025-04-11 09:19:02 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -25,8 +25,8 @@ swap_size_mib=$((swap_size * 1024))
 # Partition Layout
 parted -s "$disk" mkpart ESP fat32 1MiB 512MiB
 parted -s "$disk" set 1 esp on
-parted -s "$disk" mkpart primary ext4 512MiB -$(swap_size_mib)MiB
-parted -s "$disk" mkpart primary linux-swap -$(swap_size_mib)MiB 100%
+parted -s "$disk" mkpart primary ext4 512MiB -"${swap_size_mib}MiB"
+parted -s "$disk" mkpart primary linux-swap -"${swap_size_mib}MiB" 100%
 
 # Formatting
 mkfs.fat -F32 -n BOOT "${disk}1"
